6f93880a1e0f7a754a9ac24bc4db2f15c5637778
anirdesh <anirdesh.shankar@gmail.com>
2026-01-03 22:31:54 +0000

    scale expr results

--- refactored/runProbExpr.sh
+++ refactored/runProbExpr.sh
@@ -1,6 +1,9 @@
-for i in {4}; do
+for i in {1..4}; do
+    echo "Expr Num: $i"
+    echo "========== STARTING NEW EXPERIMENT ===================="
+
     rm -rf ./data
     rm -rf ./lora_weights
     python3 ./refactored/main.py
-    echo "Expr Num: $i"
+
 done
